6252e43206eaa3e9482f032b0598386e9612b0a8
ma.sedljarskii <ya.halo-halo@yandex.ru>
2020-09-05 16:42:03 +0400

    Улучшил 6 лабу

--- LSystems/Logic.fs
+++ LSystems/Logic.fs
@@ -1,36 +1,57 @@
 //start -> -F++F++F--
 //F -> F-F+F+F-
 
-
 module LSystems.Logic
     open System
     open System.Drawing
     
     let replaceProduction (str: string) (prod: string) : string =
-        str.Replace("F", prod)        
-                
-    let stack = [(400.0, 200.0, 0.0)]
+        str.Replace("F", prod)                
     
-    let rec draw str (g: Graphics) (start: int) (len: double) (angle: double): int =        
+    let first(c, _, _) = c
+    let second(_, c, _) = c
+    let thrid(_, _, c) = c
+    
+    let drawStack str (g: Graphics) (len: double) (angle: double) =
+        let mutable stack = [(400.0, 500.0, 90.0)]
         let blackPen = new Pen(Color.Black, 1.5f)
         
-        let mutable currentAngle = 0.0
-        let mutable x = 400.0
-        let mutable y = 200.0
-        
         let size = (str |> String.length) - 1
-        let mutable i = start
         
-        while i < size do       
+        for i = 0 to size do
+            let currentAngle = thrid stack.Head
+            let x = first stack.Head
+            let y = second stack.Head
+            
             match str.[i] with
             | 'F' ->                
                 let newX = len * (cos (currentAngle * Math.PI / 180.0)) + x
                 let newY = len * (-sin (currentAngle * Math.PI / 180.0)) + y                
-                g.DrawLine(blackPen, float32 x,  float32 y, float32 newX, float32 newY)
-                x <- newX
-                y <- newY
-            | '+' -> currentAngle <- (currentAngle + angle)
-            | '-' -> currentAngle <- (currentAngle - angle)                
-            
-            i <- i + 1        
-        i
+                g.DrawLine(blackPen, float32 x,  float32 y, float32 newX, float32 newY)                
+                stack <- (newX, newY, currentAngle) :: stack.Tail                 
+            | '+' -> stack <- (x, y, currentAngle + angle) :: stack.Tail
+            | '-' -> stack <- (x, y, currentAngle - angle) :: stack.Tail
+            | '[' -> stack <- stack.Head :: stack
+            | ']' -> stack <- stack.Tail
+        
+    
+    
+//    let draw str (g: Graphics) (len: double) (angle: double) =        
+//        let blackPen = new Pen(Color.Black, 1.5f)
+//        
+//        let mutable currentAngle = 0.0
+//        let mutable x = 400.0
+//        let mutable y = 200.0
+//        
+//        let size = (str |> String.length) - 1
+//                
+//        for i = 0 to size do       
+//            match str.[i] with
+//            | 'F' ->                
+//                let newX = len * (cos (currentAngle * Math.PI / 180.0)) + x
+//                let newY = len * (-sin (currentAngle * Math.PI / 180.0)) + y                
+//                g.DrawLine(blackPen, float32 x,  float32 y, float32 newX, float32 newY)
+//                x <- newX
+//                y <- newY
+//            | '+' -> currentAngle <- (currentAngle + angle)
+//            | '-' -> currentAngle <- (currentAngle - angle)     
--- LSystems/Program.fs
+++ LSystems/Program.fs
@@ -10,14 +10,14 @@ let main argv =
     //start -> -F++F++F--
     //F -> F-F+F+F-
         
-    let mutable str = "-F++F++F--"        
-    for i = 1 to 4 do
-        str <- replaceProduction str "F-F+F+F-"
+    let mutable str = "F"        
+    for i = 1 to 3 do
+        str <- replaceProduction str "F[+FF][-FF]F[-F][+F]F"
     
     let img = new Bitmap(800, 800)    
     let g  = Graphics.FromImage(img)     
     
-    draw str g 0 10.0 90.0 |> ignore
+    drawStack str g 15.0 36.0
     
     let temp = new Form() in
     temp.Paint.Add(fun e -> e.Graphics.DrawImage(img, 0, 0))
